6d195ad020f90af665e30209c3522a8233b9ade1
OmarTamer177 <Otamer95@gmail.com>
2024-06-25 19:03:21 +0300

    Initialize Marcy Voice assistant

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import time
+import playsound
+import speech_recognition as rs
+from gtts import gTTS
+from google.cloud import texttospeech
+
+
+def speak(text):
+    audio = gTTS(text=text, lang='en')
+    audio.save('audio.mp3')
+    playsound.playsound('audio.mp3')
+    os.remove('audio.mp3')
+
+
+speak('Hi, I am Marcy!, your virtual assistant!, how can I help you?')
