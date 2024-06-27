bde47b17b460846884b2dbc2e5bc8b28cb91ee1e
OmarTamer177 <Otamer95@gmail.com>
2024-06-27 21:28:12 +0300

    added functions to get date and time

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
 import speech_recognition as sr
 import pyttsx3
-import pywhatkit
-import wikipedia
+import datetime
+import pywhatkit as pwk
+import wikipedia as wiki
 
 
 class Assistant:
     def __init__(self):
-        # Initialize the recognizer and TTS engine
+        # Initialize the Speech Recognizer and Text To Speech engines
         self.recognizer = sr.Recognizer()
         self.tts_engine = pyttsx3.init()
 
-        # Give marcy a suitable voice
+        # Give Marcy a suitable voice
         voices = self.tts_engine.getProperty('voices')
         self.tts_engine.setProperty('voice', voices[1].id)
 
@@ -37,22 +38,32 @@ class Assistant:
 
     # Function to take text input (for testing purpose)
     def take_command_text(self):
-        command = input()
+        command = input("Me: ")
         return command
 
     # Function to handle the commands
     def handle_command(self, command):
-        if 'bye' in command:
+        # Exit the app
+        if 'bye' in command or 'quit' in command or 'exit' in command:
             self.talk("see you soon!")
             sys.exit()
+        # Getting the time in hours and minutes
         elif 'time' in command:
-            self.talk("time is right now")   # Dummy code to be replaced
+            self.talk("time is " + datetime.datetime.now().strftime("%H:%M"))
+        # Getting today's date
+        elif 'date' in command:
+            self.talk(datetime.datetime.now().strftime("%A, %d %B %Y"))
+        # 3shan el 3yal el 3beta (to be removed)
+        elif 'i love you' in command or 'i like you' in command:
+            self.talk("Unfortunately, I am a robot and I don't possess human emotions")
+        # In case the command wasn't handled
         else:
-            self.talk("I Can't understand you")
+            self.talk("Sorry, I Can't understand you")
 
 
 if __name__ == "__main__":
     Marcy = Assistant()
     Marcy.talk("Hi, I am Marcy, your virtual assistant, how can I help you?")
     while True:
-        Marcy.handle_command(Marcy.take_command_text())
+        Marcy.handle_command(Marcy.take_command_text().lower())
+        Marcy.talk("What else can I help you with?")
